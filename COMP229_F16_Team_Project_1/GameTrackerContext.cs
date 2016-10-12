827833d874a3c33d67f59db5855feb422c1828d6
Thiago Andrade <thiagoh@gmail.com>
2016-10-11 21:05:59 -0400

    database model

---
+++ COMP229_F16_Team_Project_1/GameTrackerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using COMP229_F16_Team_Project_1.Models;
+
+namespace COMP229_F16_Team_Project_1 {
+    public class GameTrackerContext : DbContext {
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Week> Week { get; set; }
+    }
+}
--- COMP229_F16_Team_Project_1/Models/Week.cs
+++ COMP229_F16_Team_Project_1/Models/Week.cs
@@ -6,7 +6,6 @@ using System.Web;
 namespace COMP229_F16_Team_Project_1.Models {
     public class Week {
 
-        public string ID { get; set; }
         public string number { get; set; }
 
     }
