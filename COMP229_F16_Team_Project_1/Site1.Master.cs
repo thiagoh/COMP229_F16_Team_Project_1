c1cb6426895df554af943097b78f143a575f9361
Thiago Andrade <thiagoh@gmail.com>
2016-10-18 12:58:19 -0400

    ownin initial setup

--- COMP229_F16_Team_Project_1/Site1.Master.cs
+++ COMP229_F16_Team_Project_1/Site1.Master.cs
@@ -5,13 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace COMP229_F16_Team_Project_1
-{
-    public partial class Site1 : System.Web.UI.MasterPage
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
+namespace COMP229_F16_Team_Project_1 {
+    public partial class Site1 : System.Web.UI.MasterPage {
+
+        public bool loggedIn { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack) {
+
+                // check is a user is logged in
+                loggedIn = HttpContext.Current.User.Identity.IsAuthenticated;
+            }
         }
     }
 }
---
+++ COMP229_F16_Team_Project_1/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+// required for OWIN Startup
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(COMP229_F16_Team_Project_1.Startup))]
+
+namespace COMP229_F16_Team_Project_1 {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+
+            var authOpts = new CookieAuthenticationOptions();
+            authOpts.AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie;
+            authOpts.LoginPath = new PathString("/Login.aspx");
+
+            app.UseCookieAuthentication(authOpts);
+        }
+    }
+}
